f515936ff06df37f559e0f0969cbe3d0985d67aa
Jordan Smith <Jordan.smith@alumniserv.com>
2024-10-01 07:46:47 +0200

    Added update endpoint

---
+++ src/DotnetTests.Api/Features/Weather/UpdateWeatherEntry/UpdateWeatherEntry.cs
@@ -0,0 +1,45 @@
+﻿using dotnet_tests.Features.Weather;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotnetTests.Api.Features.Weather.UpdateWeatherEntry;
+
+public class UpdateWeatherEntry(MyDbContext _context, ILogger<UpdateWeatherEntry> _logger) 
+    : Endpoint<UpdateWeatherEntryRequest, Results<NotFound, NoContent>, UpdateWeatherEntryMapper>
+{
+    public override void Configure()
+    {
+        Put("");
+        Group<WeatherApiGroup>();
+        AllowAnonymous();
+    }
+
+    public override async Task<Results<NotFound, NoContent>> ExecuteAsync(UpdateWeatherEntryRequest req, CancellationToken ct)
+    {
+        var entity = Map.ToEntity(req);
+
+        try
+        {
+            var dbItem = await _context.WeatherEntries
+                .FirstOrDefaultAsync(x => x.Id == entity.Id, ct);
+
+            if (dbItem is null)
+            {
+                return TypedResults.NotFound();
+            }
+
+            _context.Entry(dbItem).CurrentValues.SetValues(entity);
+
+            await _context.SaveChangesAsync(ct);
+
+            return TypedResults.NoContent();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "An error has occurred when updating an entry");
+            return TypedResults.NotFound();
+        }
+    }
+}
+
+public record UpdateWeatherEntryRequest(int Id, DateTime Date, string Summary, double Temperature);
---
+++ src/DotnetTests.Api/Features/Weather/UpdateWeatherEntry/UpdateWeatherEntryMapper.cs
@@ -0,0 +1,17 @@
+﻿using DotnetTests.Database.Models;
+
+namespace DotnetTests.Api.Features.Weather.UpdateWeatherEntry;
+
+public class UpdateWeatherEntryMapper : RequestMapper<UpdateWeatherEntryRequest, WeatherEntry>
+{
+    public override WeatherEntry ToEntity(UpdateWeatherEntryRequest r)
+    {
+        return new WeatherEntry
+        {
+            Id = r.Id,
+            Date = r.Date,
+            Summary = r.Summary,
+            TemperatureCelsius = (decimal) r.Temperature
+        };
+    }
+}
